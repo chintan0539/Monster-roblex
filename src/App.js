e09e562faa24fa998549664c774d565323800a4b
Chintan0539 <chintanpatel0539@gmail.com>
2021-08-17 11:22:53 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -6,24 +6,20 @@ class App extends Component {
     super();
 
     this.state = {
-      monsters: [
-        {
-          name: "Chintan",
-        },
-        {
-          name: "Yash",
-        },
-        {
-          name: "rupam",
-        },
-      ],
+      monsters: [],
     };
+  }
+
+  componentDidMount() {
+    fetch("https://jsonplaceholder.typicode.com/users").then((response) =>
+      response.json().then((users) => this.setState({ monsters: users }))
+    );
   }
   render() {
     return (
       <div className="App">
         {this.state.monsters.map((monster, k) => (
-          <h1 key={k}>{monster.name}</h1>
+          <h1 key={monster.id}>{monster.name}</h1>
         ))}
       </div>
     );
