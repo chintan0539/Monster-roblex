91cbda7fc4a41417b0f30c5cd926fb784a896033
Chintan0539 <chintanpatel0539@gmail.com>
2021-08-28 14:30:18 +0530

    search component seperated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { Component } from "react";
 import "./App.css";
 import { CardList } from "./components/card-list/card-list.component";
-
+import Searchbox from "./components/search-box/search-box.component";
 class App extends Component {
   constructor() {
     super();
@@ -17,20 +17,23 @@ class App extends Component {
       response.json().then((users) => this.setState({ monsters: users }))
     );
   }
+  handleChange = (e) => {
+    this.setState({ searchField: e.target.value });
+  };
   render() {
     const { monsters, searchField } = this.state;
+
+    // function to filter search
     const filteredMonsters = monsters.filter((monster) =>
       monster.name.toLowerCase().includes(searchField.toLowerCase())
     );
     return (
       <div className="App">
-        <input
-          type="search"
+        <Searchbox
           placeholder="search Monsters"
-          onChange={(e) => {
-            this.setState({ searchField: e.target.value });
-          }}
+          handleChange={this.handleChange}
         />
+
         <CardList monsters={filteredMonsters}></CardList>
       </div>
     );
